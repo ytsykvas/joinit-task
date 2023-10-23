48f6f4475817c192b6def72dc9e2ebd98965c953
Yurii Tsykvas <yurii@lucalabs.com>
2023-10-23 11:39:20 +0300

    Barcode checker

---
+++ barcode_checker.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# convert number to ISBN13 barcode
+class BarcodeChecker
+
+  def self.number_to_isbn(number)
+    return 'wrong ISBN' unless valid_isbn?(number)
+
+    digits = number.to_s.chars.map(&:to_i)
+    sum = digits.each_with_index.sum { |digit, index| digit * (index.even? ? 1 : 3) }
+    check_digit = (10 - (sum % 10)) % 10
+    number + check_digit.to_s
+  end
+
+  def self.valid_isbn?(number)
+    number.to_s.match?(/^\d{12}$/)
+  end
+end
+
+isbn = BarcodeChecker.number_to_isbn('978014300723')
+puts "ISBN: #{isbn}" if isbn
